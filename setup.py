c3e31d4db2e514cdeda25bab88ce14bda33a6680
Andres Gonzalez Fuentes <andres.gonzalez.f@ug.uchile.cl>
2021-10-31 13:17:53 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An study of the performance of multiple period estimation methods',
+    author='Andres Gonzalez',
+    license='MIT',
+)
